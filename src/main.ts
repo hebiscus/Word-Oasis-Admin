04a5fdbb1a5863aea0dece08f69d86f468f8d276
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-09-05 21:23:20 +0200

    uninstall lexical

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import './styles/style.scss';
 
+
+const editor = createEditor(config);
+
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     I'm innocent I swear!
   </div>
 `
+\
